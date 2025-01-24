6cbca3c819db69c05688b09ab9a79779b8d25846
adtiya <mittaladitya0801@gmail.com>
2025-01-24 12:23:55 +0530

    Factorial of a number

---
+++ Factorial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter the number you want to find the factorial of: ";
+    cin >> num;
+    for (int i = num-1; i < num && num >= 1 && i >=1 ; i--){
+        num = num*i;
+    }
+    cout << "The factorial of the number is: " << num << '\n';
+}
